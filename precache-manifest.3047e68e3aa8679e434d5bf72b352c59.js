fb29e19de64152f20362856f87d3f8227d4fcc9c
Jai Minocha <56990377+jaiminocha@users.noreply.github.com>
2022-08-17 18:18:44 +0530

    Updates

---
+++ precache-manifest.3047e68e3aa8679e434d5bf72b352c59.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "072fd48955241dd6d2afd9a3b3596eb7",
+    "url": "/Keeper-App-React-17/index.html"
+  },
+  {
+    "revision": "731e4757498e8894696a",
+    "url": "/Keeper-App-React-17/static/js/2.5dc2e22d.chunk.js"
+  },
+  {
+    "revision": "e11cf1dfec21000f56f1",
+    "url": "/Keeper-App-React-17/static/js/main.721cc8db.chunk.js"
+  },
+  {
+    "revision": "ea01269c779396373a0f",
+    "url": "/Keeper-App-React-17/static/js/runtime-main.3ea4e74f.js"
+  }
+]);
